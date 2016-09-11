c3597f4fe09de3a28b100d1158f5b72a5d20d594
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2016-09-11 18:00:11 +0530

    Voice changed to female.

--- src/main/webapp/customer/Controllers/ConversationController.js
+++ src/main/webapp/customer/Controllers/ConversationController.js
@@ -12,6 +12,7 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
     $scope.location = false; // disable map display at the begining
     $scope.activeImage = $scope.advertisements[0];
     let activeImageIndex = 0;
+    let voice;
 
     // Floor map props
     let selectedSVGColor = null;
@@ -33,55 +34,54 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
     };
 
 
-        if (!('webkitSpeechRecognition' in window)) {
+    if (!('webkitSpeechRecognition' in window)) {
 //        upgrade();
-        } else {
-            recognition = new webkitSpeechRecognition();
-            recognition.continuous = true;
-            recognition.interimResults = true;
+    } else {
+        recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
 
-            recognition.onstart = function () {
-                console.log('Started')
-            };
+        recognition.onstart = function () {
+            console.log('Started')
+        };
 
-            recognition.onresult = function (event) {
-                let interim_transcript = '';
+        recognition.onresult = function (event) {
+            let interim_transcript = '';
 
-                for (let i = event.resultIndex; i < event.results.length; ++i) {
-                    final_transcript = '';
-                    interim_transcript = '';
-                    if (event.results[i].isFinal) {
-                        final_transcript += event.results[i][0].transcript;
+            for (let i = event.resultIndex; i < event.results.length; ++i) {
+                final_transcript = '';
+                interim_transcript = '';
+                if (event.results[i].isFinal) {
+                    final_transcript += event.results[i][0].transcript;
 
-                        $('#chat-input').val(final_transcript);
-                        $scope.sendClick();
-                    } else {
-                        interim_transcript += event.results[i][0].transcript;
-                        $('#chat-input').val(interim_transcript);
-                    }
+                    $('#chat-input').val(final_transcript);
+                    $scope.sendClick();
+                } else {
+                    interim_transcript += event.results[i][0].transcript;
+                    $('#chat-input').val(interim_transcript);
                 }
-            };
+            }
+        };
 
-            recognition.onerror = function (event) {
-            };
+        recognition.onerror = function (event) {
+        };
 
-            recognition.onend = function () {
-                console.log('Finished');
-            };
-        }
+        recognition.onend = function () {
+            console.log('Finished');
+        };
+    }
 
-        $scope.speak = function (text) {
-            var msg = new SpeechSynthesisUtterance();
-            var voices = window.speechSynthesis.getVoices();
-            msg.voice = voices[10]; // Note: some voices don't support altering params
-            msg.voiceURI = 'native';
-            msg.volume = 1; // 0 to 1
-            msg.rate = 1; // 0.1 to 10
-            msg.pitch = 2; //0 to 2
-            msg.text = text;
-            msg.lang = 'en-US';
-            speechSynthesis.speak(msg);
-        }
+    $scope.speak = function (text) {
+        var msg = new SpeechSynthesisUtterance();
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Google UK English Female'; })[0];
+        msg.voiceURI = 'native';
+        msg.volume = 1; // 0 to 1
+        msg.rate = 1; // 0.1 to 10
+        msg.pitch = 1; //0 to 2
+        msg.text = text;
+        msg.lang = 'en-US';
+        speechSynthesis.speak(msg);
+    }
 
     $scope.init = function () {
         $scope.svg = Snap('#svgdiv');
@@ -133,12 +133,14 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
     $scope.customerMsg = function (msg) {
         $scope.conversation.push({ai: false, msg: msg});
         $scope.scroll();
+        $scope.addPriorityExpire();
     };
 
     $scope.aiMsg = function (msg) {
         $scope.conversation.push({ai: true, msg: msg});
         $scope.speak(msg);
         $scope.scroll();
+        $scope.addPriorityExpire();
     };
 
     $scope.scroll = function () {
@@ -167,7 +169,7 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
                 $scope.conversation = [];
                 $scope.$apply();
             }
-        }, 20000);
+        }, 2000000);
     };
 
     $scope.markFloor = function (svgId) {
@@ -188,7 +190,6 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
         }
     };
 
-    $scope.addPriorityExpire();
     $scope.updateAdds();
     $scope.start();
 
@@ -200,5 +201,5 @@ angular.module("lei-admin").controller("ConversationController", function ($scop
         }
         $scope.activeImage = $scope.advertisements[activeImageIndex];
         $scope.$apply();
-    }, 2000);
+    }, 10000);
 });
